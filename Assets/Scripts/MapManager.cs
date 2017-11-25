3569f584c28ad1b592bb368baff8d66d2a5d74a3
Edmond Zalenski <ezalen2@uic.edu>
2017-11-25 11:21:00 -0600

    Base map generation (#1)
    * add test script and basic map data
    
    * meta data
    
    * map organization with nodes
    
    * draw roads
    
    * draw map better
    
    * textured simple roads
    
    * filter non-vehicle ways
    
    * add test script and basic map data
    
    * meta data
    
    * map organization with nodes
    
    * draw roads
    
    * draw map better
    
    * textured simple roads
    
    * filter non-vehicle ways

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour {
+    public string xmlFileName;
+
+    // Use this for initialization
+    void Start () {
+        string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, xmlFileName);
+
+        GameObject navMapObj = new GameObject("NavMap");
+        navMapObj.transform.position = new Vector3(0, 0, 0);
+        navMapObj.AddComponent<NavMap>();
+        var navMap = navMapObj.GetComponent<NavMap>();
+        navMap.setMap(filePath);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
---
+++ Assets/Scripts/Way.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Way : MonoBehaviour {
+    public long id;
+    private Dictionary<string, string> tags = new Dictionary<string, string>();
+    public List<long> refs = new List<long>();
+    public string type;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    public void addRef(long id) {
+        refs.Add(id);
+    }
+
+    public void addTags(ArrayList tags) {
+        foreach(ArrayList tag in tags) {
+            string key = (string)tag[0];
+            string val = (string)tag[1];
+            if(key == "highway") type = val;
+            this.tags.Add(key, val);
+        }
+    }
+}
