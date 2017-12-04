5967273907abdcdb30a2ae41665378034dd6913f
Jon Hopp <jhopp3@uic.edu>
2017-12-04 04:55:46 -0600

    Initially set the lights to a random state

--- Assets/Scripts/Light.cs
+++ Assets/Scripts/Light.cs
@@ -16,12 +16,23 @@ public class Light : MonoBehaviour {
     public Light(Vector3 pos, Transform tf) {
         position = pos;
         lightTransform = tf;
+        // Initially set the lights to a random state.
+        lightState = (LightState)Math.Round(UnityEngine.Random.value);
     }
     public void draw() {
         GameObject light = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        light.GetComponent<Renderer>().material.color = Color.red;
         light.transform.parent = lightTransform;
         light.transform.position = position;
         light.transform.localScale = new Vector3(SCALE_FACTOR, SCALE_FACTOR, SCALE_FACTOR);
+        switch (lightState) {
+            case LightState.RED_GREEN:
+                light.GetComponent<Renderer>().material.color = Color.red;
+                break;
+            case LightState.GREEN_RED:
+                light.GetComponent<Renderer>().material.color = Color.green;
+                break;
+            default:
+                break;
+        }
     }
 }
