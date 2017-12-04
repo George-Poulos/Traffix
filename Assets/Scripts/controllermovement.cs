42b842cae66f86d53f2e851c8090ec86a582fb31
ConditionalCoder <Georgepoulos5@gmail.com>
2017-12-03 23:49:09 -0600

    Adding Movement

--- Assets/Scripts/controllermovement.cs
+++ Assets/Scripts/controllermovement.cs
@@ -6,7 +6,7 @@ using Valve.VR;
 // // from answers.unity3d.com/questions/1188342
 
 
-public class VRTouchpadMove : MonoBehaviour
+public class controllermovement : MonoBehaviour
 {
 	public GameObject player;
 
@@ -15,8 +15,8 @@ public class VRTouchpadMove : MonoBehaviour
 
 	Vector2 touchpad;
 
-	float sensitivityX = 0.75F;
-	float sensitivityForward = 1.5F;
+	float sensitivityX = 0.5F;
+	float sensitivityForward = 1.0F;
 	private Vector3 playerPos;
 
 	void Start()
@@ -28,22 +28,26 @@ public class VRTouchpadMove : MonoBehaviour
 	void Update()
 	{
 		device = SteamVR_Controller.Input((int)controller.index);
+		//If finger is on touchpad
 		if (device.GetTouch(SteamVR_Controller.ButtonMask.Touchpad))
 		{
+			//Read the touchpad values
 			touchpad = device.GetAxis(EVRButtonId.k_EButton_SteamVR_Touchpad);
 
 
+			// Handle movement via touchpad
 			if (touchpad.y > 0.2f || touchpad.y < -0.2f) {
 				// Move Forward
 				player.transform.position += controller.transform.forward * Time.deltaTime * (touchpad.y * sensitivityForward);
 
 			}
 
+			// handle rotation via touchpad
 			if (touchpad.x > 0.3f || touchpad.x < -0.3f) {
 				player.transform.Rotate (0, touchpad.x * sensitivityX, 0);
-			
 			}
 
+			Debug.Log ("Touchpad X = " + touchpad.x + " : Touchpad Y = " + touchpad.y);
 		}
 	}
 }
