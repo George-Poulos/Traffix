307a22b2286614aedc35da999b64516020236197
Edmond Zalenski <ezalen2@uic.edu>
2017-12-04 08:32:57 -0600

    added spawn system

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class MapManager : MonoBehaviour {
     public string xmlFileName;
+    public GameObject prefab;
 
     private GameObject path;
     private GameObject navMapObj;
@@ -18,46 +19,12 @@ public class MapManager : MonoBehaviour {
         navMapObj.transform.position = new Vector3(0, 0, 0);
         navMapObj.AddComponent<NavMap>();
         var navMap = navMapObj.GetComponent<NavMap>();
+        navMap.spawnPrefab = prefab;
         navMap.setMap(filePath);
-        paths = navMap.getPaths(929515174).FindAll(delegate(NavPath p) { return p.isPath; });
-        path = new GameObject("Path");
-        path.AddComponent<LineRenderer>();
-        InvokeRepeating("SpawnThing", 0f, 5f);
     }
 
     // Update is called once per frame
     void Update () {
 
-    }
-
-    void SpawnThing() {
-        var p = RandomPath().First();
-        var line = path.GetComponent<LineRenderer>();
-        List<Vector3> points = p.getPoints();
-        // Set the width of the Line Renderer
-        line.SetWidth(0.01F, 0.01F);
-        // Set the number of vertex fo the Line Renderer
-        line.positionCount = points.Count;
-        for(int i = 0; i < points.Count; i++) {
-            line.SetPosition(i, points[i]+(Vector3.up * .01f));
-        }
-        GameObject car = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        car.name = "Car";
-        car.transform.localScale = new Vector3(.1f,.1f,.1f);
-        var move = car.AddComponent<MoveCrap>();
-        car.transform.position = navMapObj
-            .GetComponent<NavMap>()
-            .mapObj.GetComponent<Map>()
-            .Nodes[929515174]
-            .gameObject.transform.position;
-        move.positions = points.ToArray();
-    }
-
-    public IEnumerable<NavPath> RandomPath() {
-        System.Random rand = new System.Random();
-        while(true)
-        {
-            yield return paths[rand.Next(paths.Count)];
-        }
     }
 }
