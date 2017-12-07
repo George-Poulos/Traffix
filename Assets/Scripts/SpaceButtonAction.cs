7c2913fee1fb0da44607c57e63290bb3cd81ceb2
ConditionalCoder <Georgepoulos5@gmail.com>
2017-12-06 23:38:03 -0600

    added hide buildings

--- Assets/Scripts/SpaceButtonAction.cs
+++ Assets/Scripts/SpaceButtonAction.cs
@@ -3,22 +3,32 @@ using System.Collections;
 
 public class SpaceButtonAction : MonoBehaviour {
 
-        void Update() {
-                if (Input.GetKeyDown ("a")) {
-                        print ("space key was pressed");
-                        GameObject nav = GameObject.Find ("NavMap");
-                        if(nav != null) Destroy (nav);
-                        MapManager mapManager = GameObject.Find("Map Manager").GetComponent<MapManager>();
-                        mapManager.xmlFileName = "Berlin.xml";
-                        mapManager.init ();
-                }
-                if (Input.GetKeyDown ("b")) {
-                        print ("space key was pressed");
-                        GameObject nav = GameObject.Find ("NavMap");
-                        if(nav != null) Destroy (nav);
-                        MapManager mapManager = GameObject.Find("Map Manager").GetComponent<MapManager>();
-                        mapManager.xmlFileName = "London.xml";
-                        mapManager.init ();
-                }
+	private bool viewBuildings = true;
+
+    void Update() {
+        if (Input.GetKeyDown ("a")) {
+                print ("space key was pressed");
+                GameObject nav = GameObject.Find ("NavMap");
+                if(nav != null) Destroy (nav);
+                MapManager mapManager = GameObject.Find("Map Manager").GetComponent<MapManager>();
+                mapManager.xmlFileName = "Berlin.xml";
+                mapManager.init ();
         }
+        if (Input.GetKeyDown ("b")) {
+                print ("space key was pressed");
+                GameObject nav = GameObject.Find ("NavMap");
+                if(nav != null) Destroy (nav);
+                MapManager mapManager = GameObject.Find("Map Manager").GetComponent<MapManager>();
+                mapManager.xmlFileName = "London.xml";
+                mapManager.init ();
+        }
+		if (Input.GetKeyDown ("q")) {
+			var bUnits = GameObject.FindObjectsOfType<BuildingRenderer>();
+	 		Debug.Log ("Hello");
+			viewBuildings = !viewBuildings;
+			foreach (var b in bUnits) {
+				b.gameObject.SetActive (viewBuildings);
+			}
+		}
+	}
 }
